11b94dc658167fd1c0859ac485e2873b15c7d507
Lukas <lukas_benner@web.de>
2020-03-17 14:52:35 +0100

    .

--- test_spacy.py
+++ test_spacy.py
@@ -1,8 +1,10 @@
 import spacy
 from spacy import displacy
 from collections import Counter
+import talon 
+
 
 nlp = spacy.load("en_core_web_sm")
 
-doc = nlp('European authorities fined SWG a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
+doc = nlp('European authorities fined L. Müller a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
 print([(X.text, X.label_) for X in doc.ents])
